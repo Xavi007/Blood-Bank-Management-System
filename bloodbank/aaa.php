d931d407120867a93c58814288eedf0828752292
Xavi007 <93143666+Xavi007@users.noreply.github.com>
2022-08-28 19:41:21 -0700

    Add files via upload

---
+++ bloodbank/aaa.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<?php $title="Bloodbank | Available Blood Samples"; ?>
+<?php require 'head.php'; ?>
+<body>
+    <?php require 'header.php'; ?>
+    <div class="container cont">
+		<div class="row justify-content-center">
+				<a class="navbar-brand" href="index.php"><img src="image/dd.jpg" width="60" height="60" class="rounded-circle">𝐁𝐋𝐎𝐎𝐃 𝐃𝐎𝐍𝐀𝐓𝐈𝐎𝐍 𝐂𝐀𝐌𝐏𝐀𝐈𝐆𝐍𝐒</a>
+              
+        </div>	
+			</div>
+				<div class="row justify-content-center">
+				<form method="get" action="" style="margin-top:-20px; ">
+			<table class="table table-responsive table-striped rounded-center">
+				<tr><th colspan="7" class="title">Available Blood Samples</th></tr>
+				<tr>
+					<th>#</th>
+					<th>Hospital Name</th>
+					<th>Hospital Address</th>
+					<th>Hospital Phone</th>
+					<th>Donation Date</th>
+				</tr>
+			<tr>
+				<th>01</th>
+				<th>BIMS HOSPITAL </th>
+				<th>BELGAVI</th>
+				<th>9988556677</th>
+				<th>30-01-2022</th>
+			</tr>
+			<tr>
+				<th>02</th>
+				<th>K.L.E HOSPITAL </th>
+				<th>BELGAVI</th>
+				<th>9980976557</th>
+				<th>05-02-2022</th>
+			</tr>
+			<tr>
+				<th>03</th>
+				<th>DISTRICT HOSPITAL </th>
+				<th>BELGAVI</th>
+				<th>9988559997</th>
+				<th>09-02-2022</th>
+			</tr>
+			<tr>
+				<th>04</th>
+				<th>K.L.E HOSPITAL </th>
+				<th>PUNE</th>
+				<th>9988556677</th>
+				<th>10-02-2022</th>
+			</tr>
+			<tr>
+				<th>05</th>
+				<th>SDM HOSPITAL </th>
+				<th>DHARWARD</th>
+				<th>9986456677</th>
+				<th>06-03-2022</th>
+			</tr>
+			<tr>
+				<th>06</th>
+				<th>SAI HOSPITAL</th>
+				<th>BANGLORE</th>
+				<th>9977554433</th>
+				<th>07-03-2022</th>
+			</tr>
+			<tr>
+				<th>07</th>
+				<th>CITY HOSPITAL </th>
+				<th>MUMBAI</th>
+				<th>9998654333</th>
+				<th>22-03-2022</th>
+			</tr>
+	
+			</tr>
+
+        <?php ?>
+        </table>
+    </div>
+    <?php require 'footer.php' ?>
+</body>
+
+</body>
+</html>
+
+
---
+++ bloodbank/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer text-center bg-light">
+	<span class="brand">Blood Bank Management System by Xavier Dias. </span>
+	<script>
+		date = new Date();
+    	document.getElementById("demo").innerHTML=date.getFullYear();
+    </script>
+</footer>
